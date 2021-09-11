2c24e08fa138832694ddb281a095f53e1c4ab815
qichengxxxx <2484691281@qq.com>
2021-09-11 18:18:43 +0800

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: Qichengxxxx
+ * @Date: 2021-09-04 18:30:47
+ * @LastEditors: Qichengxxxx
+ * @Software: VScode
+ * @FilePath: \汕淘宝\app.js
+ */
+//app.js
+App({
+  //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
+  onLaunch: function (options) {},
+  onShow: function (options) {},
+  onHide: function () {},
+  onError: function (msg) {},
+  //options(path,query,isEntryPage)
+  onPageNotFound: function (options) {},
+});
+<<<<<<< HEAD
+//test
+=======
+>>>>>>> 8b1949f (master)
---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: Qichengxxxx
+ * @Date: 2021-09-04 18:30:47
+ * @LastEditors: Qichengxxxx
+ * @Software: VScode
+ * @FilePath: \汕淘宝\pages\index\index.js
+ */
+//Page Object
+Page({
+  data: {},
+  //options(Object)
+  onLoad: function (options) {},
+  onReady: function () {},
+  onShow: function () {},
+  onHide: function () {},
+  onUnload: function () {},
+  onPullDownRefresh: function () {},
+  onReachBottom: function () {},
+  onShareAppMessage: function () {},
+  onPageScroll: function () {},
+  //item(index,pagePath,text)
+  onTabItemTap: function (item) {},
+});
